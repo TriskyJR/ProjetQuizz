a9f73107b75f52b9ae2ef659561a1a4b4fc42fe1
filipe <filipe_1018@hotmail.com>
2020-02-03 11:06:45 +0100

    Formulaire question et réponse

--- Quiz/src/Controller/QuizController.php
+++ Quiz/src/Controller/QuizController.php
@@ -6,7 +6,13 @@ use App\Repository\TAnswerRepository;
 use App\Repository\TQuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\Session;
+use App\Entity\TAnswer;
+use App\Entity\TQuestion;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class QuizController extends AbstractController
 {
@@ -46,4 +52,96 @@ class QuizController extends AbstractController
     {
         return $this->render('quiz/login.html.twig');
     }
+    /**
+     * @Route("/new", name="new")
+     */
+    public function create(TQuestion $question = null,Request $request, ManagerRegistry $managerRegistry)
+    {
+
+        if(!$question){
+            $question = new TQuestion();
+        }
+        
+        $form = $this->createFormBuilder($question)
+                     ->add('queTitle', TextType::class, [
+                         'label' => "Énoncé de la question",
+                         'attr' => [
+                             'placeholder' => "La maman d'alex"
+                         ]
+                     ])
+                     ->add('queType', ChoiceType::class, [
+                         'label' => "Type de question",
+                         'choices' => [
+                             'Radio bouton' => true,
+                             'Checkbox' => false
+                         ],
+                         'choice_label' => function($choice, $key, $value){
+                            return strtoupper($key);
+                         },
+                     ])
+                     ->getForm();
+
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $managerRegistry->getManager();
+            $em->persist($question);
+            $em->flush();
+            return $this->redirectToRoute('createAnswer');
+        }
+
+
+        return $this->render('quiz/createQuestion.html.twig',[
+            'formQuestion' => $form->createView(),
+
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="createAnswer")
+     */
+    public function createAnswer(TAnswer $answer = null, TQuestionRepository $repo ,Request $request, ManagerRegistry $managerRegistry){
+
+        if(!$answer){
+            $answer = new TAnswer();
+        }
+
+        $question = $repo->findOneBy([], ['id' => 'desc']);
+
+        $formAnswer = $this->createFormBuilder($answer)
+                           ->add('ansTitle', TextType::class, [
+                                'label' => "Réponse",
+                                'attr' => [
+                                    'placeholder' => 'Le CLient'
+                                ]
+                            ])
+                            ->add('ansTrueFalse', ChoiceType::class, [
+                                'label' => "Réponse juste ou fausse",
+                                'choices' => [
+                                    'vrai' => true,
+                                    'faux' => false
+                                ],
+                                'choice_label' => function($choice, $key, $value){
+                                    return strtoupper($key);
+                                },
+                            ])
+                            ->getForm();
+
+        $formAnswer->handleRequest($request);
+
+        if($formAnswer->isSubmitted() && $formAnswer->isValid()){
+            $em = $managerRegistry->getManager();
+            $answer->setQuestion($question);
+            $em->persist($question);
+            $em->flush();
+            return $this->redirectToRoute('createAnswer');
+        }
+
+        return $this->render('quiz/createAnswer.html.twig', [
+            'question' => $question,
+            'formAnswer' => $formAnswer->createView(),
+        ]);
+    }
+
 }
